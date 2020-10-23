526cc9d37c6fe7274840a02a1f31a3fc8cdc1aec
Maria Kot <kotkotmasha@gmail.com>
2020-10-23 19:11:07 +0300

    23.10.2020

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {Menu} from "./components/Menu";
+import {Route,BrowserRouter} from "react-router-dom";
+import {Pages} from "./pages/Pages";
+import {AddPage} from "./pages/AddPage";
+import {EditPage} from "./pages/EditPage";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  return (<div className="container-fluid">
+          <BrowserRouter>
+          <div className="row">
+              <Menu/>
+              <div class="col-9">
+                <Route exact path="/" render={()=>"CMS"}/>
+                <Route exact path="/pages" render={()=><Pages/>}/>
+                <Route path="/pages/addPage" render={()=><AddPage/>}/>
+                <Route path="/pages/editPage" render={()=><EditPage/>}/>
+              </div>
+          </div>
+          </BrowserRouter>
+      </div>
+
   );
 }
 
---
+++ src/pages/AddPage.jsx
@@ -0,0 +1,118 @@
+import React, {createRef} from "react";
+import AceEditor from "react-ace";
+import "emmet-core";
+import "ace-builds/src-noconflict/mode-html";
+import "ace-builds/src-noconflict/mode-css";
+import "ace-builds/src-noconflict/mode-javascript";
+import "ace-builds/src-noconflict/theme-vibrant_ink";
+import "ace-builds/src-noconflict/ext-emmet"
+
+
+export class AddPage extends React.Component{
+    constructor() {
+        super();
+        this.htmlEditor = createRef();
+        this.cssEditor = createRef();
+        this.jsEditor = createRef();
+        this.handleSave = this.handleSave.bind(this);
+        this.handleInputChange = this.handleInputChange.bind(this);
+        this.state = {
+            name: "",
+            title: ""
+
+        }
+    }
+    handleSave(){
+        let formData = new FormData();
+        formData.append('name',this.state.name);
+        formData.append('title',this.state.title);
+        formData.append( 'html',this.htmlEditor.current.editor.getValue());
+        formData.append('css',this.cssEditor.current.editor.getValue());
+        formData.append('js',this.jsEditor.current.editor.getValue());
+
+        fetch("http://mkot.kotkotm1.beget.tech/addPage", {
+            method: 'POST',
+            body: formData
+    })
+        .then(response=>response.json())
+            .then(result=>console.log(result))
+    }
+    componentDidMount() {
+        console.log("Вызвана функция");
+    }
+
+    handleInputChange(event){
+        const target = event.target;
+        const value = target.value;
+        const name = target.name;
+
+        this.setState({
+            [name]: value
+        })
+    }
+
+    render() {
+        return <div>
+            <div className="nav nav-tabs" id="nav-tab" role="tablist">
+            <a className="nav-link active" id="nav-html-tab" data-toggle="tab" href="#nav-html" role="tab"
+                   aria-controls="home" aria-selected="true">HTML</a>
+                <a className="nav-link" id="nav-css-tab" data-toggle="tab" href="#nav-css" role="tab"
+                   aria-controls="nav-css" aria-selected="false">CSS</a>
+                <a className="nav-link" id="nav-js-tab" data-toggle="tab" href="#nav-js" role="tab"
+                   aria-controls="nav-js" aria-selected="false">JS</a>
+                <a className="nav-link" id="nav-extraHTML-tab" data-toggle="tab" href="#nav-extraHTML" role="tab"
+                   aria-controls="nav-extraHTML" aria-selected="false">Параметры</a>
+                <button onClick={this.handleSave} className="btn btn-light ml-auto">[сохранить]</button>
+            </div>
+        <div className="tab-content" id="myTabContent">
+            <div className="tab-pane fade show active" id="nav-html" role="tabpanel" aria-labelledby="nav-html-tab">
+                <AceEditor
+                    mode="html"
+                    width="100%"
+                    theme="vibrant_ink"
+                    ref={this.htmlEditor}
+                    setOptions={{
+                        fontSize:18,
+                        enableEmmet:true
+                    }}
+                />
+            </div>
+            <div className="tab-pane fade" id="nav-css" role="tabpanel" aria-labelledby="nav-css-tab">
+                <AceEditor
+                    mode="css"
+                    width="100%"
+                    theme="vibrant_ink"
+                    ref={this.cssEditor}
+                    setOptions={{
+                        fontSize:18,
+                        enableEmmet:true
+                    }}
+                />
+            </div>
+            <div className="tab-pane fade" id="nav-js" role="tabpanel" aria-labelledby="nav-js-tab">
+                <AceEditor
+                    mode="javascript"
+                    width="100%"
+                    theme="vibrant_ink"
+                    ref={this.jsEditor}
+                    setOptions={{
+                        fontSize:18,
+                        enableEmmet:true
+                    }}
+                />
+            </div>
+            <div className="tab-pane fade" id="nav-extraHTML" role="tabpanel" aria-labelledby="nav-extraHTML-tab">
+                <div className="col-10 mx-auto my-3">
+                    <div className="mb-3">
+                        <input name="name" onChange={this.handleInputChange} type="text" className="form-control" placeholder="URI страницы"/>
+                    </div>
+                    <div className="mb-3">
+                        <input name="title" onChange={this.handleInputChange} type="text" className="form-control" placeholder="Заголовок страницы"/>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        </div>
+    }
+}
